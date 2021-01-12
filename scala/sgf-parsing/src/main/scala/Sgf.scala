9d49551a24d132ec307ec222b699d89ee3839c21
vpletuhin <qwertyzmail@gmail.com>
2021-01-12 17:30:24 +0300

    sgf parser impl

--- scala/sgf-parsing/src/main/scala/Sgf.scala
+++ scala/sgf-parsing/src/main/scala/Sgf.scala
@@ -4,7 +4,7 @@ object Sgf extends RegexParsers {
 
   type Tree[A] = Node[A] // to separate the type from the constructor, cf. Haskell's Data.Tree
   type Forest[A] = List[Tree[A]]
-  case class Node[A](rootLabel: A, subForest: Forest[A] = List())
+  case class Node[A](root: A, subForest: Forest[A] = List())
 
   // A tree of nodes.
   type SgfTree = Tree[SgfNode]
@@ -13,5 +13,23 @@ object Sgf extends RegexParsers {
   // Keys may have multiple values associated with them.
   type SgfNode = Map[String, List[String]]
 
-  def parseSgf(text: String): Option[SgfTree] = ???
+  def parseSgf(text: String): Option[SgfTree] = {
+    parseAll(tree, text) match {
+      case Success(r, _) => Some(r)
+      case _ => None
+    }
+  }
+
+  def tree: Parser[SgfTree] = "(" ~ node ~ forest ~ ")" ^^ {case _ ~ root ~ sf ~ _ => Node(root, sf)}
+  def forest: Parser[Forest[SgfNode]] = singleChild | rep(tree)
+  def singleChild: Parser[Forest[SgfNode]] = node ^^ (x => List(Node(x)))
+  def node: Parser[SgfNode] = ";" ~ rep(property) ^^ {case _ ~ x => x.toMap}
+  def property: Parser[(String, List[String])] = key ~ rep1(value) ^^ {case k ~ v => (k,v)}
+  def key: Parser[String] = "[A-Z]{1,2}".r
+  def value: Parser[String] = "\\[[A-Za-z0-9]+\\]".r.map(_.drop(1).dropRight(1))
+
+  def main(args: Array[String]): Unit = {
+    val r = parseAll(tree, "(;FF[4](;B[aa];W[ab])(;B[dd];W[ee]))")
+    println(r)
+  }
 }
