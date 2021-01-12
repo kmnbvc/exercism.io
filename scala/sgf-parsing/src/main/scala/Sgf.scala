1c7723c58b837c07d9ef3f55e7e5962b518c5089
vpletuhin <qwertyzmail@gmail.com>
2021-01-13 00:37:20 +0300

    sgf parser minor changes

--- scala/sgf-parsing/src/main/scala/Sgf.scala
+++ scala/sgf-parsing/src/main/scala/Sgf.scala
@@ -13,6 +13,8 @@ object Sgf extends RegexParsers {
   // Keys may have multiple values associated with them.
   type SgfNode = Map[String, List[String]]
 
+  override def skipWhitespace: Boolean = false
+
   def parseSgf(text: String): Option[SgfTree] = {
     parseAll(tree, text) match {
       case Success(r, _) => Some(r)
@@ -20,13 +22,18 @@ object Sgf extends RegexParsers {
     }
   }
 
-  def tree: Parser[SgfTree] = "(" ~ node ~ forest ~ ")" ^^ {case _ ~ root ~ sf ~ _ => Node(root, sf)}
+  def tree: Parser[SgfTree] = "(" ~> node ~ forest <~ ")" ^^ { case root ~ sf => Node(root, sf) }
   def forest: Parser[Forest[SgfNode]] = singleChild | rep(tree)
   def singleChild: Parser[Forest[SgfNode]] = node ^^ (x => List(Node(x)))
-  def node: Parser[SgfNode] = ";" ~ rep(property) ^^ {case _ ~ x => x.toMap}
-  def property: Parser[(String, List[String])] = key ~ rep1(value) ^^ {case k ~ v => (k,v)}
+  def node: Parser[SgfNode] = ";" ~> rep(property) ^^ (_.toMap)
+  def property: Parser[(String, List[String])] = key ~ rep1(value) ^^ { case k ~ v => (k, v) }
   def key: Parser[String] = "[A-Z]{1,2}".r
-  def value: Parser[String] = "\\[[A-Za-z0-9]+\\]".r.map(_.drop(1).dropRight(1))
+  def value: Parser[String] = "[" ~> valueData <~ "]"
+  def valueData: Parser[String] = rep1(escapedNewline | escapedChar | whitespace | normalChar) ^^ (_.mkString)
+  def escapedNewline: Parser[String] = """\\\n""".r ^^^ ""
+  def whitespace: Parser[String] = "\\s".r ^^^ " "
+  def escapedChar: Parser[String] = "\\" ~> ".".r
+  def normalChar: Parser[String] = """[^\\\]]""".r
 
   def main(args: Array[String]): Unit = {
     val r = parseAll(tree, "(;FF[4](;B[aa];W[ab])(;B[dd];W[ee]))")
